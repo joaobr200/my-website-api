24e119a6a7328a038232ed6577aae94236ac304a
Joaobr200 <joaoarantesjob@gmail.com>
2020-10-04 05:44:33 -0300

    add body parser

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import bodyParser from "body-parser";
 
 import routes from "./Routes";
 import userRoutes from "./Routes/users";
 
 const app = express();
-app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", routes);
 app.use("/users", userRoutes);
